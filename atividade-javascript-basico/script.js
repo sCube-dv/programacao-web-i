aa1aeb8ffc098a0b9f7ab9427ebf719fe35a3954
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-01-06 23:40:17 -0300

    Adicionada lótiga para preencher um vetor e retornar a posição de um determinado elemento deste.

--- atividade-javascript-basico/script.js
+++ atividade-javascript-basico/script.js
@@ -3,16 +3,34 @@ let itemMenu = document.querySelectorAll('.item-menu')
 
 /* Função que adiciona a classe 'ativo' no item de menu selecionado */
 function selectedItem() {
-    itemMenu.forEach ((item) => 
+    itemMenu.forEach((item) =>
         item.classList.remove('ativo')
     )
     this.classList.add('ativo')
 }
 
-/* Pega o click do item selecionado e executa a função 'selectedLink' */ 
-itemMenu.forEach((item) => 
+/* Pega o click do item selecionado e executa a função 'selectedLink' */
+itemMenu.forEach((item) =>
     item.addEventListener('click', selectedItem)
 )
+
+
+/* Lógica para exibir e ocultar as telas principais */
+/* funções para mostrar as telas de acordo com a opção de menu selecionada */
+const mostrarHome = () => {
+
+    // alert('home')
+    let home = document.getElementsByClassName('home')
+
+    if(home.style.display === 'none'){
+        // home.style.display = 'block'
+        alert('home')
+    } else {
+        home.style.display = 'none'
+    }
+    
+}
+
 
 /* Lógica para calcular o fatorial do número informado */
 
@@ -23,7 +41,7 @@ const btnCalc = document.getElementById('calcFat')
 btnCalc.addEventListener('click', () => {
     let fat = parseInt(n.value) /* converte para numero int */
 
-    /* Verifica se é um valor válido */
+    /* Verifica se é um valor válido (número >= 0) */
     if (isNaN(fat) || fat < 0) {
         alert("Por favor, insira um número inteiro não negativo")
         return
@@ -33,11 +51,84 @@ btnCalc.addEventListener('click', () => {
     for (let i = fat; i > 1; i--) {
         resultado *= i
     }
-    
+
     document.getElementById('fatRes').innerHTML = `O fatorial do número ${fat} é <strong>${resultado}</strong> `
 })
 
 const limpar = () => document.getElementById('fatRes').innerHTML = ''
+
+
+
+/* Lógica para calcular a multiplicação entre os vetores */
+
+const nElemVet = document.getElementById('nElemVet')
+
+let vetA = []
+let vetB = []
+let vetRes = []
+
+const preencherVetor = document.getElementById('preencherVetor')
+
+preencherVetor.addEventListener('click', () => {
+
+    let nElem = parseInt(nElemVet.value)
+    alert(`Ok! Cada Vetor terá ${nElem} elementos! Vamos Preenchê-los...`)
+
+    for (let i = 0; i < nElem; i++) {
+        vetA[i] = parseInt(prompt(`Infome o ${i+1}° valor para o Vetor A:`))
+    } 
+    for (let i = 0; i < nElem; i++) {
+        vetB[i] = parseInt(prompt(`Infome o ${i+1}° valor para o Vetor B:`))
+    } 
+    for (let i = 0; i < nElem; i++) {
+        vetRes[i] = vetA[i]*vetB[i]
+    } 
+
+    document.querySelector('.vetA').innerText = `Vetor A: [${vetA}]`
+    document.querySelector('.vetB').innerText = `Vetor A: [${vetB}]`
+    document.querySelector('.vetRes').innerText = `Vetor A*B: [${vetRes}]`
+})
+
+const limparVet = () => {
+    document.querySelector('.vetA').innerText = ''
+    document.querySelector('.vetB').innerText = ''
+    document.querySelector('.vetRes').innerText = ''
+}
+
+/* Lógica para Preencher e Retornar a Posição de um Item em um Vetor */
+
+const qtdElemVet = document.getElementById('qtdElemVet')
+
+let Vet = []
+
+const povoarVetor = () => {
+    let vNelem = parseInt(qtdElemVet.value)
+
+    alert(`Ok! O Vetor terá ${vNelem} elementos! Vamos Preenchê-los...`)
+
+    for (let i = 0; i < vNelem; i++) {
+        Vet[i] = parseInt(prompt(`Infome o ${i+1}° valor para o Vetor`))
+    }
+
+    return document.querySelector('.exibVet').innerText = `Vetor [${Vet}]`
+}
+
+const elemPos = document.getElementById('elemPos')
+
+const mostraPosElem = () => {
+    let posElem = parseInt(elemPos.value)
+
+    for(let i = 0; i < Vet.length; i++){
+
+        if (Vet[i] === posElem) {
+            return document.querySelector('.posItemVet').innerHTML = `O elemento '<strong>${posElem}</strong>' ocupa a posição '<strong>${i}</strong>' no <strong>Vetor [${Vet}]</strong>`
+        }
+
+        return document.querySelector('.posItemVet').innerHTML = `O elemento '<strong>${posElem}</strong>' não pertence ao <strong>Vetor [${Vet}]</strong>`
+    }
+        
+}
+
 
 
 /* Lógica para Expandir o Menu */
